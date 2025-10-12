c2b18bbe0c8caa042059da3fb22a372130a149a6
Alexey Rudnitskiy <almax1024@gmail.com>
2025-10-12 15:14:14 +0300

    11.10.2025 -- Polished General EDA Part

---
+++ model.py
@@ -0,0 +1,16 @@
+import abc
+
+
+
+class Model():
+    def __init__():
+        pass
+
+    @abstractmethod
+    def validate_data():
+        pass
+
+    @abstractmethod
+    def predict():
+        pass
+
